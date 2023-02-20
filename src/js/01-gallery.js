45fd54cc14db2989aaa9bb6cbd1bf5f34f6c4ddd
Maryna-Harasko <mngarasko@gmail.com>
2023-02-20 20:57:53 +0200

    task 1, 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const imgCardMarkup = createImgCardMarkup(galleryItems);
+
+function createImgCardMarkup(galleryItems){return galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery__item"><a class="gallery__link" href = "${original}">
+    <img 
+        class="gallery__image"
+        src="${preview}" 
+        alt = "${description}"
+        width = "340"></img></a></div>`
+    }).join("")};
+
+galleryEl.insertAdjacentHTML("afterbegin", imgCardMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+});
